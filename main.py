cff41c69b6377e53b6b08f604a8beebc53bcd217
Ankita Verma <ankita.av.934@gmail.com>
2025-04-06 19:25:40 +0530

    html pages in cat and dog, voting, weather route changes

--- main.py
+++ main.py
@@ -1,100 +1,102 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Form, Query
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 import requests
+from typing import Optional
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 API_KEY = "d3924a9862061bdd58299ef7ee0e5f42"
 
+cat_votes = 0
+dog_votes = 0
+
 @app.get("/")
-def root():
-    return {"message": "Welcome to my webpage"}
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.get("/dogs")
-def show_dogs():
+def show_dogs(request: Request):
     response = requests.get('https://dog.ceo/api/breeds/image/random')
     image_url = response.json()['message']
 
-    html_content = f""" 
-    <html>
-        <head><title>Random Dog Image</title></head>
-        <body>
-            <img src="{image_url}" alt='Dog Image' width='300'/>
-        </body>
-    </html>"""
-
-    return HTMLResponse(content=html_content)
+    return templates.TemplateResponse("animal.html", {"request": request, "image_url": image_url, "title": "Dog Image", "animal": "dog"})
 
 @app.get("/cats")
-def show_cats():
+def show_cats(request: Request):
     response = requests.get('https://api.thecatapi.com/v1/images/search')
     image_url = response.json()[0]['url']
 
-    html_content = f""" 
-    <html>
-        <head><title>Random Cat Image</title></head>
-        <body>
-            <img src="{image_url}" alt='Cat Image' width='300'/>
-        </body>
-    </html>"""
-
-    return HTMLResponse(content=html_content)
+    return templates.TemplateResponse("animal.html", {"request": request, "image_url": image_url, "title": "Cat Image", "animal": "cat"})
 
 @app.get("/both")
-def show_animals():
-    print(API_KEY)
+def show_animals(request: Request):
     r_dog = requests.get('https://dog.ceo/api/breeds/image/random')
     image_dog = r_dog.json()['message']
 
     r_cat = requests.get('https://api.thecatapi.com/v1/images/search')
     image_cat = r_cat.json()[0]['url']
 
-    html_content = f""" 
-    <html>
-        <head><title>Random Cat and Dog Image</title></head>
-        <body>
-            <h1>This is a cat</h1>
-            <img src="{image_cat}" alt='Cat Image' width = '300' />
+    return templates.TemplateResponse("both.html", {
+        "request": request, 
+        "image_dog": image_dog, 
+        "image_cat": image_cat,
+        "cat_votes": cat_votes,
+        "dog_votes": dog_votes})
 
-            <h1>This is a dog</h1>
-            <img src="{image_dog}" alt='Dog Image' width='300'/>
-        </body>
-    </html>"""
-    
-    return HTMLResponse(content=html_content)
+@app.post("/vote")
+def vote(animal: str = Form(...)):
+    global cat_votes, dog_votes
 
+    if animal == "cat":
+        cat_votes += 1
+    elif animal == "dog":
+        dog_votes += 1
 
-@app.get("/weather/{city}")
-def show_weather(city: str):
-    coord = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}")
+    return RedirectResponse(url="/both", status_code=303)
+
+@app.get("/weather/", response_class=HTMLResponse)
+def show_weather(request: Request, city: Optional[str] = Query(default=None)):
+    if city is None:
+        return templates.TemplateResponse("weather.html", {
+            "request": request,
+            "city": None,
+            "temp": None,
+            "hum": None,
+            "sea_level": None,
+            "wind_speed": None
+        })
+    import urllib.parse
+    encoded_city = urllib.parse.quote(city)
+
+    coord = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={encoded_city}&limit=1&appid={API_KEY}")
     
     coord_data = coord.json()
     if not coord_data:
-        return {"error": "City doesn't exist"}
+        return templates.TemplateResponse("weather.html", {
+            "request": request,
+            "error": f"No results found for city {city}",
+            "city": city
+        })
     
     lat = coord_data[0]['lat']
     lon = coord_data[0]['lon']
 
     weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric")
     weather_data = weather.json()
-    if not weather_data:
-        return {"error": "Invalid"}
     
     temp = weather_data['main']['temp']
     hum = weather_data['main']['humidity']
     sea_level = weather_data['main']['sea_level']
     wind_speed = weather_data['wind']['speed']
 
-    html_content = f""" 
-    <html>
-        <head><title>Weather Forecast</title></head>
-        <body>
-            <h1>Weather conditions of {city} are:<h1>
-            Temperature = {temp} deg C<br>
-            Humidity = {hum} % <br>
-            Sea Level = {sea_level} Pa<br>
-            Wind Speed = {wind_speed} km/h <br>
-        </body>
-    </html>"""
-    
-    return HTMLResponse(content=html_content)
+    return templates.TemplateResponse("weather.html", 
+                                      {"request": request, 
+                                       "city": city, 
+                                       "temp": temp, 
+                                       "hum": hum, 
+                                       "sea_level": sea_level, 
+                                       "wind_speed": wind_speed})
 
